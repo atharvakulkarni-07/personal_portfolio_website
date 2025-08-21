9fc6acd1c72ab3013af90a9da614c1a4095b7cfd
atharvakulkarni-07 <atharvakulkarni.official@gmail.com>
2025-08-21 17:33:48 +0530

    changes made in project section

--- src/data/experienceData.tsx
+++ src/data/experienceData.tsx
@@ -1,5 +1,16 @@
 export const experienceData = [
   {
+    title: "Software Development Engineer Intern",
+    company: "AI24C",
+    date: "Jun 2025 - July 2025",
+    location: "Bengaluru, Karnataka",
+    responsibilities: [
+      "Reduced per-document delay by 20% by designing and optimizing an AI-Powered document digitization pipeline.",
+      "Orchestrated a complete voice command functionality to generate complex agentic AI workflows.",
+      "Structured a scalable and normalized database schema comprising 10+ entities for a multi-tenant system",
+    ],
+  },
+  {
     title: "AI Research & Development Intern",
     company: "Atom360 Pvt. Ltd.",
     date: "Dec 2024 - Feb 2025",
